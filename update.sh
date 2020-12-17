8e3f35e9b2810ebe4919c6a51f336423b80cc427
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2020-12-17 18:14:06 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 zCh=$(curl https://raw.githubusercontent.com/ZechBron/zVirus-Gen/zVirus/update)
 
 update() {
-echo -e "\e[32mDo you want to update it? (y/n)\e[0m"
+echo -e "\e[32mDo you want to update it? (y/n) \e[0m"
 read ChB
 
 if [ "$ChB" = "y" ]; then
@@ -12,8 +12,10 @@ rm -rf zVirus-Gen
 git clone https://github.com/ZechBron/zVirus-Gen
 cd zVirus-Gen
 chmod +x setup.sh
-chmod +x zVirus
 chmod +x update.sh
+chmod +x zVirus.py
+bash setup.sh
+cd
 elif [ "$ChB" = "n" ]; then
 echo "Ok"
 else
@@ -22,47 +24,43 @@ fi
 
 }
 
-if [ "$zCh" = "Version 1.1.0" ]; then
+if [ "$zCh" = "Version 1.1.1" ]; then
 echo -e "\e[91mNo latest version available\e[0m"
-
-elif [ "$zCh" = "Version 1.1.1" ]; then
-echo -e "\e[92mVersion 1.1.1 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.2" ]; then
-echo -e "\e[92mVersion 1.1.2 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.3" ]; then
-echo -e "\e[92mVersion 1.1.3 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.4" ]; then
-echo -e "\e[92mVersion 1.1.4 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.5" ]; then
-echo -e "\e[92mVersion 1.1.5 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.6" ]; then
-echo -e "\e[92mVersion 1.1.6 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.7" ]; then
-echo -e "\e[92mVersion 1.1.7 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.8" ]; then
-echo -e "\e[92mVersion 1.1.8 available\e[0m"
-update
-
-elif [ "$zCh" = "Version 1.1.9" ]; then
-echo -e "\e[92mVersion 1.1.9 available\e[0m"
-update
 
 elif [ "$zCh" = "Version 1.2" ]; then
 echo -e "\e[92mVersion 1.2 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.3" ]; then
+echo -e "\e[92mVersion 1.3 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.4" ]; then
+echo -e "\e[92mVersion 1.4 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.5" ]; then
+echo -e "\e[92mVersion 1.5 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.6" ]; then
+echo -e "\e[92mVersion 1.6 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.7" ]; then
+echo -e "\e[92mVersion 1.7 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.8" ]; then
+echo -e "\e[92mVersion 1.8 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 1.9" ]; then
+echo -e "\e[92mVersion 1.9 available\e[0m"
+update
+
+elif [ "$zCh" = "Version 2.0" ]; then
+echo -e "\e[92mVersion 2.0 available\e[0m"
 update
 
 else
